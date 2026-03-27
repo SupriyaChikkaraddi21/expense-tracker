ea1ff40e375183609f08e6c1730d5d86e4b69c4b
Supriya Chikkaraddi <supriyachikkaraddi206@gmail.com>
2026-03-27 22:49:28 +0530

    p3

--- expense-tracker/src/main.tsx
+++ expense-tracker/src/main.tsx
@@ -6,6 +6,7 @@ import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    console.log("CLIENT ID:", import.meta.env.VITE_GOOGLE_CLIENT_ID);
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
       <App />
     </GoogleOAuthProvider>
